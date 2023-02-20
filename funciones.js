f6dbd9ee5b7656e11f574172793670305679afe5
Car-fuentes <carolinainesfuentes@gmail.com>
2023-02-19 21:30:40 -0300

    camsbios presentacion

--- funciones.js
+++ funciones.js
@@ -1,9 +1,9 @@
 const fortalezas = [
-"<bold>Persistencia</bold>: si la tercera no es la vencida la vigésima lo puede ser.",
-"<bold>Pensamiento lateral</bold>: a todo se le puede encontrar la vuelta.",
-"<bold>Paciencia</bold>: el tiempo que se necesite.",
-"<bold>Voluntad</bold>: se hace lo que se tenga que hacer.",
-"<bold>Pensamiento positivo</bold>: nunca es tarde pero cuanto antes mejor."]
+"<span>Persistencia</span>: si la tercera no es la vencida la vigésima lo puede ser.",
+"<span>Pensamiento lateral</span>: a todo se le puede encontrar la vuelta.",
+"<span>Paciencia</span>: el tiempo que se necesite.",
+"<span>Voluntad</span>: se hace lo que se tenga que hacer.",
+"<span>Pensamiento positivo</span>: nunca es tarde pero cuanto antes mejor."]
 let fLen = fortalezas.length;
 
 let text = "<ul>";
